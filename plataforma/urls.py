e37956a4b7643fb982ce6f79f3e409d0b522ba13
ClaudioFritzen <sdfritzen96@gmail.com>
2022-07-12 00:58:38 -0300

    valida dados pacientes

--- plataforma/urls.py
+++ plataforma/urls.py
@@ -9,8 +9,8 @@ from . import views
 urlpatterns = [
 
     path('pacientes/', views.pacientes, name="pacientes"),
-    path('dados_pacientes/', views.dados_paciente_listar, name="dados_paciente_listar"),
-    #path('dados_pacientes/<str:id>/', views.dados_paciente, name="dados_paciente"),
+    path('dados_paciente/', views.dados_paciente_listar, name="dados_paciente_listar"),
+    path('dados_pacientes/<str:id>/', views.dados_paciente, name="dados_paciente")
 
 
 ]
