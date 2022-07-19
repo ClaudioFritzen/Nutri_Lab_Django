6c365cceb8d274d101f891baad16b882c59e1171
ClaudioFritzen <sdfritzen96@gmail.com>
2022-07-19 17:27:53 -0300

    colocando grafico linear

--- plataforma/urls.py
+++ plataforma/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 
     path('pacientes/', views.pacientes, name="pacientes"),
     path('dados_paciente/', views.dados_paciente_listar, name="dados_paciente_listar"),
-    path('dados_pacientes/<str:id>/', views.dados_paciente, name="dados_paciente")
-
+    path('dados_pacientes/<str:id>/', views.dados_paciente, name="dados_paciente"),
+    path('grafico_peso/<str:id>/', views.grafico_peso, name="grafico_peso")
 
 ]
