6a9355cd0f5896f8fd0ff8e1276a5142f70675d3
ClaudioFritzen <sdfritzen96@gmail.com>
2022-07-21 14:14:01 -0300

    adionando o plano alimentar pt1

--- plataforma/urls.py
+++ plataforma/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
 
     path('pacientes/', views.pacientes, name="pacientes"),
     path('dados_paciente/', views.dados_paciente_listar, name="dados_paciente_listar"),
-    path('dados_pacientes/<str:id>/', views.dados_paciente, name="dados_paciente")
-
+    path('dados_pacientes/<str:id>/', views.dados_paciente, name="dados_paciente"),
+    path('grafico_peso/<str:id>/', views.grafico_peso, name="grafico_peso"),
+    path('plano_alimentar_listar/', views.plano_alimentar_listar, name="plano_alimentar_listar"),
+    path('plano_alimentar/<str:id>/', views.plano_alimentar, name="plano_alimentar")
 
 ]
